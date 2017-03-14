1ce136ce988f414bde9ad4437fd8cd61cf12fba5
j-Sut <flowstategame@gmail.com>
2017-03-14 10:25:43 -0700

    submit form working

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //	1 Single State object: 
-
 var state = {
 	items: []
 }
@@ -7,9 +6,13 @@ var state = {
 //	2 functions that modify state: 
 
 var addItem = function(state, item) {
-	state.items.push(items);
+	state.items.push(item);
+	console.log(item);
 
 };
+
+//var checkedItem = function() {};
+//var deletedItem = function() {};
 
 // 3 Functions that render state:
 // The render functions should all take two arguments: 
@@ -18,14 +21,21 @@ var addItem = function(state, item) {
 var renderList = function(state, element) {
 	var itemsHTML = state.items.map(function(item) {
 		return '<li>' + item + '</li>';
-	};)
+	});
 	element.html(itemsHTML)
+
 };
 
 // 4 Event listeners
 
-$('.shopping-list-add').submit(function(event) {
+$('#js-shopping-list-form').submit(function(event) {
+	console.log("fired inside event listener");
+
 	event.preventDefault();
-	addItem(state, $('.shopping-list-add-input').val());
+	addItem(state, $("#shopping-list-entry").val());
 	renderList(state, $('.shopping-list'));
+	console.log(addItem);
+	console.log("hello")
 });
+
+
